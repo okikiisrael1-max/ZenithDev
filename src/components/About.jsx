65e6af1821403d6cbf071a361a82c5547c0169d0
Zenith Web Dev <okikiisrael1@gmail.com>
2026-01-01 17:35:57 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
     <div id='about'>
       <Title Title={'About Me'} Desc={'A Little About Me'}/>
       <div className='flex flex-col sm:flex-row px-[10%] gap-5'>
-        <img className='rounded-lg object-cover w-62.5 max-h-80 flex mx-auto sm:w-120 grayscale-75 hover:grayscale-0 transition' src={assets.hero} alt="" />
+        <img className='rounded-lg z-10 object-cover w-62.5 max-h-80 flex mx-auto sm:w-120 grayscale-75 hover:grayscale-0 transition' src={assets.hero} alt="" />
         <div className='flex flex-col gap-2.5 z-1'>
             <p className='text-gray-400'>I’m passionate about turning ideas into functional, beautiful web applications. With expertise in <span className='text-transparent font-bold bg-linear-60 from-30% from-purple-600 to-blue-500 bg-clip-text'>React, JavaScript, HTML, CSS, and modern frontend libraries</span> , I specialize in building responsive, user-friendly websites that meet your goals.</p>
             <p className='text-gray-400'>I thrive on <span className='text-transparent font-bold bg-linear-60 from-30% from-purple-600 to-blue-500 bg-clip-text'>problem-solving, clean code, and delivering results</span>  that make a real impact for clients and businesses alike.</p>
@@ -25,3 +25,4 @@ const About = () => {
 }
 
 export default About
+
